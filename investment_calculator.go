a34d24a2a3342d60ac665fbe1c42f4e22a3630df
Levente Krotos <krotos.levi@gmail.com>
2024-05-24 09:45:31 +0200

    first dummy project

--- investment_calculator.go
+++ investment_calculator.go
@@ -1,7 +1,26 @@
 package main
 
+import "math"
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	const inflationRate = 2.5;
+	var investmentAmount float64;
+	var expectedReturn float64;
+	var years float64;
+
+	fmt.Print("Enter the investment amount: ");
+	fmt.Scan(&investmentAmount);
+
+	fmt.Print("Enter the expected return: ");
+	fmt.Scan(&expectedReturn);
+
+	fmt.Print("Enter the years: ");
+	fmt.Scan(&years);
+
+	futureValue := investmentAmount * math.Pow(1 + expectedReturn / 100, years);
+	futureRalValue := futureValue / math.Pow(1 + inflationRate / 100, years);
+
+	fmt.Println("Future value: ", futureValue);
+	fmt.Println("Future real value: ", futureRalValue);
 }
